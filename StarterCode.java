eb294cbf79e83f7251b9b96ec2277d354a43454f
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-08-06 16:43:04 -0400

    Update StarterCode.java

--- StarterCode.java
+++ StarterCode.java
@@ -1,7 +1,33 @@
 public class First{
 
     public static void main(String []args){
-        System.out.println("HELLO!");
+        Grapefruit g = new Grapefruit();
+        g.setName("Bill");
+        g.SayHello();
+
+        Apple a = new Apple();
+        a.setName("Mary");
+        a.SayHello();
+
     }
+}
+
+class Fruit{
+        private String Name;
+        public String getName(){
+        return Name;
+    }
+    public void setName(String aName){this.Name = aName;}
+
+    public void SayHello(){
+        System.out.println(this.getName());
+    }
+}
+
+class Grapefruit extends Fruit{
+
+}
+
+class Apple extends Fruit{
 
 }
