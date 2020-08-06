ff21687836d7c8a3b9a8c9917bf310bc2ce38716
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-08-06 18:18:10 -0400

    Create LionsAndTigersV2.java

---
+++ LionsAndTigersV2.java
@@ -0,0 +1,80 @@
+import java.util.Random; 
+
+public class First{
+
+    public static void main(String []args){
+        Jungle.RunGame();
+        Jungle.ReportOnWinner();
+    }
+}
+
+class Jungle{
+    static Lion[] lions;
+    static Tiger[] tigers;
+    
+    public static int NumberOfLIONWinners = 0;
+    public static int NumberOfTigerWinners = 0;
+    
+    public static void RunGame(){
+            lions = new Lion[10];
+            tigers = new Tiger[10];
+
+        for (int i=0; i<10; i++){
+            // let's store each Feline Reference in an Array
+            lions[i] = new Lion();
+            tigers[i] = new Tiger();
+        }
+    }
+
+    public void DoCompetition(){
+        for (int i=0; i<10; i++){
+            // let's compare the strength of each set of Felines stored in our Arrays!
+            if ( (lions[i]).getStrength()  >   (tigers[i]).getStrength() ){
+                NumberOfLIONWinners++;
+            }
+            else {
+                NumberOfTigerWinners++;
+            }
+        }
+    }
+
+    public static void ReportOnWinner(){
+        System.out.println("LIONS: " + NumberOfLIONWinners);
+        System.out.println("Tigers: " + NumberOfTigerWinners);
+    }
+}
+
+class Feline{
+        public float Strength;
+
+        public void setStrength(){
+            Random rand = new Random(); 
+            this.Strength = rand.nextInt(100); 
+        }
+         
+        public float getStrength(){
+        return this.Strength;
+    }
+}
+
+class Lion extends Feline{
+        public Lion(){
+            setStrength();
+        }
+        public void setStrength(){
+            Random rand = new Random(); 
+            System.out.println("in the set strength method!");
+            this.Strength = rand.nextInt(100); 
+        }
+}
+
+class Tiger extends Feline{
+    public Tiger(){
+        setStrength();
+    }
+    public void setStrength(){
+        Random rand = new Random(); 
+        System.out.println("in the set strength method!");
+        this.Strength = rand.nextInt(100); 
+    }
+}
